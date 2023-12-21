08a0f380ef8725d474c923d8cf741ef87c646462
geek <geek@bandaumnikov.ru>
2023-12-21 14:13:15 +0300

    fix nivel 5 herencia

--- tema_5/nivel_2_3.php
+++ tema_5/nivel_2_3.php
@@ -3,36 +3,43 @@
 - Exercici 1
 Escriu un programa que defineixi una classe Shape amb un constructor que rebi com a paràmetres l'ample i alt. 
 Defineix dues subclasses; Triangle i Rectangle que heretin de Shape i que calculin respectivament l'àrea de la forma area().
+
+
+25-11-23
+
+Hola, Katia!
+
+El nivel 1 es correcto.
+
+El nivel 2, no del todo. Estás declarando varias veces las propiedades de altura y base en las subclases.
+
+El nivel 3 se extiende un poco esta situación aunque amplias de manera correcta las subclases.
+
+
+
 */
 namespace Shape;
 abstract class Shape {
-    abstract public function calculateArea();
-}
-
-class Triangle extends Shape {
-    private $base;
-    private $height;
+    protected $base;
+    protected $height;
 
     public function __construct($base, $height) {
         $this->base = $base;
         $this->height = $height;
     }
+    abstract public function calculateArea();
+}
+
+class Triangle extends Shape {
+
     public function calculateArea() {
         return 0.5 * $this->base * $this->height;
     }
 }
 
 class Rectangle extends Shape {
-    private $length;
-    private $width;
-
-    public function __construct($length, $width) {
-        $this->length = $length;
-        $this->width = $width;
-    }
-
     public function calculateArea() {
-        return $this->length * $this->width;
+        return $this->base * $this->height;
     }
 }
 
