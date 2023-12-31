536965504f11bfec30bb94ba736654d95a97727b
geek <geek@bandaumnikov.ru>
2023-12-31 21:16:27 +0300

    index.php

--- tema_7/nivel_3/index.php
+++ tema_7/nivel_3/index.php
@@ -1,8 +1,8 @@
 <?php
-
-use App\Book;
-use App\Library;
 use App\GenereEnum;
+require_once __DIR__."/app/Library.php";
+require_once __DIR__."/app/GenereEnum.php";
+require_once __DIR__."/app/Book.php";
 /* Exercici 1
 Necessitem crear un petit software per a tractament d’informació en una biblioteca. 
 Per això necessitem representar la informació d’un llibre, que té:
@@ -21,5 +21,17 @@ Retornar llibres grans (més de 500 pàgines).
 Desenvolupa mitjançant TDD aquest programa per tal de garantir que compleix totes les funcionalitats demanades per l’enunciat. 
 */
 
+$library = new \App\Library();
 
+$books =[
+    ['title' => "A Young Doctor's Notebook", 'author' => "Mikhail Bulgakov", 'genere' =>GenereEnum::Conte, 'isbn' =>"978-966-03-6215-4",'numPages' => 384],
+    ['title' => "Ward No. 6", 'author' => "Anton Chekhov", 'genere' =>GenereEnum::Conte, 'isbn' =>"978-5-517-03403-8",'numPages' => 156],
+    ['title' => "The Star Diaries",'author' => "Stanislaw Herman Lem", 'genere' =>GenereEnum::Ciencia_ficcio,'isbn' => "978-5-17-147314-3",'numPages' => 390]
+];
 
+array_map(function ($item) use ($library) {
+    $book = new \App\Book($item['title'],$item['author'], $item['genere'],  $item['isbn'], $item['numPages']);
+    $library->addBook($book);
+},$books);
+
+print_r($library->books);
